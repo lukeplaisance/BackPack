449cc4c739c1f00675b46b1b5bb4b0df0fa9bd02
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-06-26 14:45:37 -0500

    added scripts

---
+++ Assets/Scripts/BackPackEditorWindow.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum ItemNames
+{
+    Sword,
+    Shield,
+    Gun,
+    HealthKit,
+    Money,
+}
+
+public partial class BackPackEditorWindow : UnityEditor.EditorWindow
+{
+    [UnityEditor.MenuItem("Tools/Backpack")]
+    public static void Init()
+    {
+        var window = CreateInstance<BackPackEditorWindow>();
+        window.Show();
+    }
+    public ItemData itemData;
+    public System.Collections.Generic.List<Item> items = new System.Collections.Generic.List<Item>();
+    
+    public void CreateItem()
+    {
+        items.Add(new Item(Strings.SWORD));
+        items.Add(new Item(Strings.SHIELD));
+        items.Add(new Item(Strings.GUN));
+        items.Add(new Item(Strings.HEALTHKIT));
+        items.Add(new Item(Strings.MONEY));
+    }
+
+    //public void DeletItem()
+    //{
+    //    items.Remove(item);
+    //}
+
+    private void OnGUI()
+    {
+        foreach (var item in items)
+        {
+            itemData.Draw();
+            itemData.PollEvents();
+        }
+        Repaint();
+
+        switch (Event.current.type)
+        {
+            case EventType.MouseDown:
+                if (Event.current.button == 1)
+                {
+                    var gm = new UnityEditor.GenericMenu();
+                    gm.AddItem(new GUIContent("Create Item"), false, CreateItem);
+                    //gm.AddItem(new GUIContent("Delete Item"), false, DeletItem);
+                    gm.ShowAsContext();
+                }
+                break;
+        }
+    }
+}
